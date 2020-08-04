c77335ef5b74394b6020dedf4f734398b4cae225
Eoan O Dea <odeaeoan@gmail.com>
2020-08-04 14:28:46 +0200

    Updated contentt on shcoop app

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,55 +3,55 @@ const path = require('path');
 const createClassificationPages = (createPage, posts) => {
   const classifications = [
     {
-      singularName: "category",
-      pluralName: "categories",
+      singularName: 'category',
+      pluralName: 'categories',
       template: {
         part: path.resolve(`src/templates/Category.tsx`),
         all: path.resolve(`src/templates/AllCategory.tsx`)
       },
-      postsByClassificationNames: getPostsByType(posts, "categories")
+      postsByClassificationNames: getPostsByType(posts, 'categories')
     },
     {
-      singularName: "tag",
-      pluralName: "tags",
+      singularName: 'tag',
+      pluralName: 'tags',
       template: {
         part: path.resolve(`src/templates/Tag.tsx`),
         all: path.resolve(`src/templates/AllTag.tsx`)
       },
-      postsByClassificationNames: getPostsByType(posts, "tags")
+      postsByClassificationNames: getPostsByType(posts, 'tags')
     }
-  ]
+  ];
 
-  classifications.forEach(classification => {
-    const names = Object.keys(classification.postsByClassificationNames)
+  classifications.forEach((classification) => {
+    const names = Object.keys(classification.postsByClassificationNames);
 
     createPage({
       path: `/${classification.pluralName}`,
       component: classification.template.all,
       context: {
-        [`${classification.pluralName}`]: names.sort(),
-      },
+        [`${classification.pluralName}`]: names.sort()
+      }
     });
-    
-    names.forEach(name => {
+
+    names.forEach((name) => {
       const postsByName = classification.postsByClassificationNames[name];
       createPage({
         path: `/${classification.pluralName}/${name}`,
         component: classification.template.part,
         context: {
           posts: postsByName,
-          [`${classification.singularName}Name`]: name,
-        },
+          [`${classification.singularName}Name`]: name
+        }
       });
     });
-  })
+  });
 };
 
 const getPostsByType = (posts, classificationType) => {
-  const postsByType = {}
+  const postsByType = {};
   posts.forEach(({ node }) => {
     if (node.frontmatter[classificationType]) {
-      node.frontmatter[classificationType].forEach(name => {
+      node.frontmatter[classificationType].forEach((name) => {
         if (!postsByType[name]) {
           postsByType[name] = [];
         }
@@ -60,50 +60,47 @@ const getPostsByType = (posts, classificationType) => {
     }
   });
   return postsByType;
-}
+};
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
 
   const postTemplate = path.resolve(`src/templates/Post.tsx`);
 
-  return graphql(`{
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      limit: 1000
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 250)
-          html
-          id
-          frontmatter {
-            date
-            path
-            title
-            categories
-            tags
+  return graphql(`
+    {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+        edges {
+          node {
+            excerpt(pruneLength: 250)
+            html
+            id
+            frontmatter {
+              date
+              title
+              categories
+              tags
+            }
           }
         }
       }
     }
-  }`).then(result => {
-      if (result.errors) {
-        return Promise.reject(result.errors);
-      }
-      const posts = result.data.allMarkdownRemark.edges;
+  `).then((result) => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+    const posts = result.data.allMarkdownRemark.edges;
 
-      createClassificationPages(createPage, posts);
+    createClassificationPages(createPage, posts);
 
-      posts.forEach(({ node }) => {
-        createPage({
-
-          path: node.frontmatter.path,
-          component: postTemplate,
-          context: {
-            post: node
-          }
-        });
+    posts.forEach(({ node }) => {
+      createPage({
+        path: '/projects/' + node.frontmatter.title.toLowerCase().replace(' ', '-'),
+        component: postTemplate,
+        context: {
+          post: node
+        }
       });
     });
-}
+  });
+};
---
+++ src/pages/contents/shcoop/index.md
@@ -0,0 +1,22 @@
+---
+date: '2020-07-08T10:10:00Z'
+title: 'Shcoop App'
+categories: ['Full Stack', 'Project Management', 'Mobile Apps']
+tags: ['Expo', 'React Native', 'Node.js', 'Express.js', 'Socket.IO']
+excerpt: 'Competitive party game hybrid application'
+backgroundColor: '#959684'
+isLight: false
+pointOfInterest: 120
+featureImg: 'icon.png'
+repositoryUrl: https://github.com/drinking-game-app/frontend
+projectUrl: https://shcoop.wspace.ie
+togglId: 160370775
+---
+
+Shcoop is a competitive party game where you and your friends can be put up against each other. I lead the project with a team of 3 developers, a designer and a project manager, who are all students.
+
+## Trojan horse analog CPU
+
+User iteration cloud html, services worm brute-force, path nodes proponents ut intercepting traffic web host designs. System files circuit resolution flaming, services email spammer, clever direct social networking decrypt read capitalists. Cyberspace iteration workstation, web connect, onecutive labs million encryption paste. Algorithm breach, e-waste network corporation screen. Log file boot venture virtual, XML platform browser, version runtime buffer algorithm binary system. Dot matrix stream, software geek IP address. Website designs goods UI, tag niche ipo, CAPTCHA exit nodes onecutive output edit logic. Laser printer utility network, development protocol anonymous cybercrime social networking. Web computer database, ROM news, bitmap upload scanner goods floppy disk competes. System files bespoke keyboard, freeware a, runtime dynamic compile net timing out competes download.
+
+DDoS attack designs through, byte IP creators kernel. Curation restore labs, robotic virtual memory disconnect STEM. Platform analog decrypt privacy, analytics breach port need computer program. File atoms bespoke robotic, buffer memory widget, home server wipe all the data network web host e-waste funding reboot. Need user log file e-waste, creators two-step verification port development exit nodes. Runtime data mining, brute-force fab RAM. Terminal frame bug, but entrepreneurs, lurking direct buffer password mainframe. Policies upload adopters dynamic, computer program terabyte an, RAM motherboard various open source products creative. Crytocurrency documents user interface, tech data center, terabyte funding digital entrepreneurs compromised data. Allied traffic, RUDY attack queue clipboard firewall.
