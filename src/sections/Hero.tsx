f3e123f7e92bef8be32332f8e5a95c66ba1a8ccd
Eoan O Dea <odeaeoan@gmail.com>
2020-11-24 23:14:47 +0100

    Added lottie package and image on hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 import { Section } from '../components/Section';
+import { Player } from '@lottiefiles/react-lottie-player';
 
 export const Hero = () => (
   <Section id={'home'}>
+    <Player
+      src="https://assets5.lottiefiles.com/packages/lf20_akvf8jo7.json"
+      background="transparent"
+      style={{ width: '300px', height: '300px', margin: 0 }}
+      loop
+      autoplay
+    ></Player>
+
     <h1 className="title">Eoan O'Dea</h1>
 
     <p className="date">
